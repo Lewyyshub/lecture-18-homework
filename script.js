e64ca1e524cb00ffabb528634389aea9195c9217
Lewyyshub <salamashvili21@gmail.com>
2024-10-30 09:41:21 +0400

    lecture 18 homework

---
+++ script.js
@@ -0,0 +1,47 @@
+const age = prompt("შეიყვანეთ თქვენი ასაკი");
+const movieType = prompt("ჩვეულებრივი თუ 3D");
+const timeOfTheDay = prompt("დილა თუ საღამო");
+let price;
+const discountCard = prompt("ფასდაკლების ბილეთი");
+
+function AgeControl(age) {
+  switch (true) {
+    case age <= 10:
+      return 5;
+      break;
+    case age >= 10 && age <= 65:
+      return 15;
+      break;
+    case age > 65:
+      return 10;
+      break;
+    default:
+      break;
+  }
+}
+
+function ExtraPrice(price, movieType, timeOfTheDay) {
+  switch (true) {
+    case movieType === "3D":
+      price += 5;
+    case timeOfTheDay === "საღამო":
+      price += 5;
+      break;
+    default:
+      break;
+  }
+  return price;
+}
+
+function DiscountCard(price, discountCard) {
+  if (discountCard === "დიახ") {
+    price = price - 5;
+  }
+  return price;
+}
+
+price = AgeControl(age);
+price = ExtraPrice(price, movieType, timeOfTheDay);
+price = DiscountCard(price, discountCard);
+
+alert(`ბილეთის ფასი თქვენთვის არის : ${price} ლარი,`);
